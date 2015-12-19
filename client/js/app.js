c59acf5f533936120af90531382c21eeef2378e0
Jonathan Chiu <jayjaychiu@Hotmail.com>
2015-12-19 06:25:55 +0000

    v0.0.1

--- client/js/app.js
+++ client/js/app.js
@@ -1,12 +1,12 @@
 var myApp = angular.module('myApp', [
     'ngRoute',
     'ngAnimate',
+    'ui.bootstrap',
     'btford.socket-io']).
     config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
         
         $routeProvider.when('/home', {templateUrl: '/partials/home.html', controller: 'homeController'});
         $routeProvider.when('/about', {templateUrl: '/partials/about.html', controller: 'aboutController'});
-        $routeProvider.when('/contact', {templateUrl: '/partials/contact.html', controller: 'contactController'});
         $routeProvider.when('/chat', {templateUrl: '/partials/chat.html', controller: 'chatController'});
         $routeProvider.otherwise({redirectTo: '/home'});
         
